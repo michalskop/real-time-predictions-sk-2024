7730d4d7fe7feb0e27ac023d0fa5dc5b044daeb9
Michal Skop <michal.skop@kohovolit.eu>
2024-03-22 11:05:01 +0100

    update 2024-03-22T10:05:01Z

--- test/test02/extract/extractor.py
+++ test/test02/extract/extractor.py
@@ -1,4 +1,10 @@
 """Extracts data from the downloaded SUSR files."""
 
+# prepares extracted_data.csv:
+# columns: okres_code, polling_stations, counted_polling_stations, counted, voted, counted_voted, voters, votes_1, ...
+
 import pandas as pd
+
+# path
+path = "/home/michal/dev/real-time-predictions-sk-2024/test/test02/extract/"
 
